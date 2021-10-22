0b037677ad31d8c3283d9e1e1713ce79d3a4b369
Ekaterina Kubikova <kwork@fduch.ru>
2021-10-22 12:47:04 +0300

    changed property using

--- src/main/java/utils/NativeInitProperties.java
+++ src/main/java/utils/NativeInitProperties.java
@@ -3,8 +3,7 @@ package utils;
 import java.util.Properties;
 
 public class NativeInitProperties {
-    public static final String NATIVE_PROPERTY_FILE = "src/test/resources/native.app.properties";
-    public static final Properties NATIVE_APP_PROPERTIES = PropertyFileReader.init(NATIVE_PROPERTY_FILE);
+    public static final Properties NATIVE_APP_PROPERTIES = PropertyFileReader.init("src/test/resources/native.app.properties");
 
     public static final String EMAIL = NATIVE_APP_PROPERTIES.getProperty("user.email");
     public static final String PASSWORD = NATIVE_APP_PROPERTIES.getProperty("user.password");
